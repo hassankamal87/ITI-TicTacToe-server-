0edcfef0d935160c2b1f3aec81abf96ce833ca4d
mohamedadel2323 <mohamedadel2323m@gmail.com>
2023-05-06 21:24:56 +0300

    Update NetworkAccessLayer.java

--- tictactoeServer/src/tictactoeserver/services/NetworkAccessLayer.java
+++ tictactoeServer/src/tictactoeserver/services/NetworkAccessLayer.java
@@ -5,8 +5,10 @@
  */
 package tictactoeserver.services;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,6 +16,9 @@ import java.net.SocketException;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -55,8 +60,6 @@ public class NetworkAccessLayer implements Runnable {
         try {
             System.out.println("Server Closed");
             
-            
-                
             if(server != null){
                 server.close();
                 serverThread.stop();
@@ -75,10 +78,56 @@ public class NetworkAccessLayer implements Runnable {
                 System.out.println(client.getLocalAddress().toString());
                 dis = new DataInputStream(client.getInputStream());
                 ps = new PrintStream(client.getOutputStream());
+                new ClientHandler(client);
                 ps.println("Welcome From Server");
+                BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+                System.out.println(br);
+                JSONObject signupJson= (JSONObject) new JSONParser().parse(br.readLine());
+                System.out.println(signupJson);
+                
             } catch (IOException ex) {
+                Logger.getLogger(NetworkAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
                 Logger.getLogger(NetworkAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
+}
+
+
+class ClientHandler extends Thread {
+
+    DataInputStream dis;
+    PrintStream ps;
+    static Vector<ClientHandler> clientVector = new Vector<ClientHandler>();
+
+    public ClientHandler(Socket s) {
+        try {
+            dis = new DataInputStream(s.getInputStream());
+            ps = new PrintStream(s.getOutputStream());
+            clientVector.add(this);
+            start();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                sendMessageAll(dis.readLine());
+            } catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+    
+    void sendMessageAll(String msg) {
+//        for (int i = 0; i < clientVector.size(); i++) {
+//            clientVector.get(i).ps.println(msg);
+//        }
+    }
+    
 }
