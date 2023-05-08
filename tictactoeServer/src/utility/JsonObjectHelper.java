9bbc3aa6f46d9729150362c2cf78ae5932db242c
mohamedadel2323 <mohamedadel2323m@gmail.com>
2023-05-08 09:10:53 +0300

    signup in server v1

---
+++ tictactoeServer/src/utility/JsonObjectHelper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author Mohamed Adel
+ */
+public class JsonObjectHelper {
+    public static final String HEADER = "header";
+    
+    public static final String SIGNUP = "signup";
+    public static final String LOGIN = "login";
+    public static final String EMAIL = "email";
+    public static final String NAME = "name";
+    public static final String PASSWORD = "password";
+    
+    
+}
