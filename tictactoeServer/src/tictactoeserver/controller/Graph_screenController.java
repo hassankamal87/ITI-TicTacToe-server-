067f0f18c6f434d6cfdfd0cf7d313f677e1d8d98
mdanash97 <132082850+mdanash97@users.noreply.github.com>
2023-05-09 18:04:01 +0200

    Players Graph screen v2

--- tictactoeServer/src/tictactoeserver/controller/Graph_screenController.java
+++ tictactoeServer/src/tictactoeserver/controller/Graph_screenController.java
@@ -6,7 +6,10 @@
 package tictactoeserver.controller;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -19,13 +22,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import tictactoeserver.services.DataAccessLayer;
 
 /**
  * FXML Controller class
  *
  * @author AB
  */
-public class Graph_screenController implements Initializable {
+public class Graph_screenController implements Initializable  {
 
     @FXML
     private PieChart pieChart;
@@ -39,17 +43,29 @@ public class Graph_screenController implements Initializable {
     private Text offlinePlayersText;
     @FXML
     private ImageView backBtn;
+    
+    private int onlineNo;
+    private int tottalNo;
+    private int offlineNo;
 
-    /**
-     * Initializes the controller class.
-     */
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+           tottalNo = DataAccessLayer.getInstance().getAllPlayers().size();
+           onlineNo = DataAccessLayer.getInstance().getOnlinePlayers().size();
+           offlineNo = tottalNo - onlineNo;
+           onlineNum.setText(Integer.toString(onlineNo));
+           offlineNum.setText(Integer.toString(offlineNo));
+        } catch (SQLException ex) {
+            Logger.getLogger(Graph_screenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList( 
-        new PieChart.Data(onlinePlayersText.getText(), Integer.parseInt(onlineNum.getText())), 
-        new PieChart.Data(offlinePlayersText.getText(), Integer.parseInt(offlineNum.getText())));
-        
-        pieChart.setData(pieChartData);   
+        new PieChart.Data(onlinePlayersText.getText(), onlineNo), 
+        new PieChart.Data(offlinePlayersText.getText(), offlineNo));
+        pieChart.setData(pieChartData);
+        pieChart.setLabelsVisible(false);
+       
     }    
 
     @FXML
@@ -61,5 +77,6 @@ public class Graph_screenController implements Initializable {
         primaryStage.setScene(mainScene);
         
     }
+
     
 }
